1e2192a15da573cf9dd2e55f6dec911e2ff8d355
Shylesh <shylesharepelly@gmail.com>
2023-06-22 13:26:31 +0530

    second task

--- second/index.js
+++ second/index.js
@@ -1,11 +1,47 @@
 const express=require('express')
 const app = express()
+const fetch = require('node-fetch');
 
 app.listen(3000,()=>{
     console.log("server started at port 3000")
 })
 
 app.get("/numbers",async (req,res)=>{
-const url =req.query.url;
-console.log(url)
+const allurls =req.query.url;
+if (!allurls) {
+    return res.status(400).json({ error: 'url not provided' });
+  }
+  let urlList = Array.isArray(allurls) 
+  if(urlList){
+    urlList = Array.isArray(allurls) ? allurls : [url];
+  }
+  else{
+    urlList =  allurls 
+
+  }
+  //console.log(urlList)
+  let ans=[]
+  
+    await fetch(urlList[urlList.length-1], {
+        method:'GET',
+      })
+        .then(response => response.json())
+        .then(data => {
+            for(let j=0;j<data.numbers.length;j++){
+                ans.push(data.numbers[j])
+            }
+            //ans.push(data.numbers)
+            console.log(ans)
+             res.json(ans);
+         
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    
+  
+  //const uniqueList = [...new Set(ans)];
+  //console.log(uniqueList)
+  //res.json(uniqueList)
+
 })
